246480dc967aca49573fdc02030d8e8aa88854a1
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-10 09:24:54 -0300

    projetos

---
+++ projeto/Bichos.js
@@ -0,0 +1,11 @@
+class conta{
+    constructor(numero,cpf,saldo,ativa){
+        this.numero = numero;
+        this.cpf = cpf;
+        this.saldo = saldo;
+        this.ativa = ativa;
+    }
+    ativarconta(){
+        this.ativa = ture
+    }
+}
